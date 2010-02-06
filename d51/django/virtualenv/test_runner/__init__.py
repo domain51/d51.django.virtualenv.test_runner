b9614c414fa2be4608f8ff74ae2db0bb24f737fa
Travis Swicegood <development@domain51.com>
2010-02-05 21:16:07 -0600

    refactor to use new super-class run for bootstrapping

--- d51/django/virtualenv/test_runner/__init__.py
+++ d51/django/virtualenv/test_runner/__init__.py
@@ -4,16 +4,12 @@ pkg_resources.declare_namespace(__name__)
 from d51.django.virtualenv.base import VirtualEnvironment
 
 class VirtualEnvironmentTestRunner(VirtualEnvironment):
-    def run_tests(self, my_settings, app_to_test):
-        self.activate()
-        if hasattr(self.caller, 'setUp'):
-            self.caller.setUp()
-
-        self.configure_settings(my_settings)
+    def run(self, my_settings, app_to_test):
+        super(VirtualEnvironmentTestRunner, self).run(my_settings)
         self.call_command('test', app_to_test)
 
     def __call__(self, *args, **kwargs):
-        self.run_tests(*args, **kwargs)
+        self.run(*args, **kwargs)
 
 run_tests = VirtualEnvironmentTestRunner()
 
